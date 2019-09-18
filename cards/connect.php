b6da9bb3c8930edfff2f88b76b77c0b1f95fabf0
Sue Wickholm <tiina.j.seppala@gmail.com>
2019-09-18 10:00:45 +0300

    more fixes to credential username

--- cards/connect.php
+++ cards/connect.php
@@ -1,7 +1,7 @@
 <?php
 function connect_db() {
  $servername = "localhost";
- $username = "1704456";
+ $username = "a1704456";
  $password = "zeJY9J99c";
  $dbname = " a1704456";
  // Create connection
