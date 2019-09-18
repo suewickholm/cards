0bd32aff7ae935b61f57db4d3e023e9d8f0ff15e
Sue Wickholm <tiina.j.seppala@gmail.com>
2019-09-18 09:33:49 +0300

    added connect.php

---
+++ cards/connect.php
@@ -0,0 +1,18 @@
+<?php
+function connect_db() {
+ $servername = "localhost";
+ $username = "cards";
+ $password = "zeJY9J99c";
+ $dbname = " a1704456";
+ // Create connection
+
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ } else {
+ }
+ return $conn;
+}
+?>
+
